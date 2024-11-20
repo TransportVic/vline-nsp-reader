736d80dc647acbcdb0708e283ca6f911591e2522
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-20 21:31:55 +1100

    added expected test data

--- test/pdf-table-reader.test.mjs
+++ test/pdf-table-reader.test.mjs
@@ -9,12 +9,14 @@ const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const nspFP63EasternFreightPath = path.join(__dirname, 'sample-data', 'FP63 15-09-24 Eastern and S-East Freight NSP190824.pdf')
+const nspFP63EasternFreightCSV = (await fs.readFile(path.join(__dirname, 'sample-data', 'freight.csv'))).toString().split('\n').map(line => line.split(','))
 
 describe('The PDF Table Reader class', () => {
   it('Should convert a PDF buffer into an array of pages, each containing the table data', async () => {
     let tableReader = new TableReader(nspFP63EasternFreightPath)
     let pages = await tableReader.read()
-    // console.log(pages)
+
     expect(pages.length).to.equal(6)
+    expect(pages[0]).to.deep.equal(nspFP63EasternFreightCSV)
   })
 })
