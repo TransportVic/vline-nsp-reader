d9b025ca4b65449f230e6cb56e27456001f5b6f7
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-20 20:09:41 +1100

    sort before filtering

--- test/test-nsp-website-scraper.test.mjs
+++ test/test-nsp-website-scraper.test.mjs
@@ -10,6 +10,7 @@ import url from 'url'
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const nspFP61FP62 = (await fs.readFile(path.join(__dirname, 'sample-data', 'nsp-fp61-fp62.html'))).toString()
 const nspFP63 = (await fs.readFile(path.join(__dirname, 'sample-data', 'nsp-fp63.html'))).toString()
 
 describe('The NSP website scraper', () => {
@@ -20,6 +21,17 @@ describe('The NSP website scraper', () => {
       expect(await getNSPVersion()).to.deep.equal([{
         version: 'FP63',
         effective: new Date('2024-09-15')
+      }])
+    })
+
+    it('Should work with multiple NSP version being available', async () => {
+      nock(constants.VLINE_CORPORATE_HOST).get(constants.NSP_PAGE).reply(200, nspFP61FP62)
+      expect(await getNSPVersion()).to.deep.equal([{
+        version: 'FP62',
+        effective: new Date('2023-11-19')
+      }, {
+        version: 'FP61',
+        effective: new Date('2023-05-28')
       }])
     })
   })
