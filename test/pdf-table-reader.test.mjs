71172ac2fd1973bef1061343109e3158939eb4de
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-20 22:24:05 +1100

    standardise csv -> tsv

--- test/pdf-table-reader.test.mjs
+++ test/pdf-table-reader.test.mjs
@@ -9,10 +9,10 @@ const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const nspFP63EasternFreightPath = path.join(__dirname, 'sample-data', 'FP63 15-09-24 Eastern and S-East Freight NSP190824.pdf')
-const nspFP63EasternFreightCSV = (await fs.readFile(path.join(__dirname, 'sample-data', 'freight.csv'))).toString().replace(/^\uFEFF/, '').split('\n').map(line => line.split(','))
+const nspFP63EasternFreightTSV = (await fs.readFile(path.join(__dirname, 'sample-data', 'freight.tsv'))).toString().replace(/^\uFEFF/, '').split('\n').map(line => line.split('\t'))
 
 const nspFP63AlburyPath = path.join(__dirname, 'sample-data', 'FP63 15-09-24 North Eastern Standard Gauge Full Service NSP130824.pdf')
-const nspFP63AlburyCSV = (await fs.readFile(path.join(__dirname, 'sample-data', 'albury.tsv'))).toString().replace(/^\uFEFF/, '').split('\n').map(line => line.split('\t'))
+const nspFP63AlburyTSV = (await fs.readFile(path.join(__dirname, 'sample-data', 'albury.tsv'))).toString().replace(/^\uFEFF/, '').split('\n').map(line => line.split('\t'))
 
 describe('The PDF Table Reader class', () => {
   it('Should convert a PDF buffer into an array of pages, each containing the table data (freight data)', async () => {
@@ -20,7 +20,7 @@ describe('The PDF Table Reader class', () => {
     let pages = await tableReader.read()
 
     expect(pages.length).to.equal(6)
-    expect(pages[0]).to.deep.equal(nspFP63EasternFreightCSV)
+    expect(pages[0]).to.deep.equal(nspFP63EasternFreightTSV)
   })
 
   it('Should convert a PDF buffer into an array of pages, each containing the table data (albury data)', async () => {
@@ -28,6 +28,6 @@ describe('The PDF Table Reader class', () => {
     let pages = await tableReader.read()
 
     expect(pages.length).to.equal(4)
-    expect(pages[0]).to.deep.equal(nspFP63AlburyCSV)
+    expect(pages[0]).to.deep.equal(nspFP63AlburyTSV)
   })
 })
