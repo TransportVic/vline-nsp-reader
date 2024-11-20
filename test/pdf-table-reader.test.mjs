5a8434cdfe41bed8b6f6b8d11b63aa24cd060416
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-20 22:02:20 +1100

    albury test data

--- test/pdf-table-reader.test.mjs
+++ test/pdf-table-reader.test.mjs
@@ -11,12 +11,23 @@ const __dirname = path.dirname(__filename)
 const nspFP63EasternFreightPath = path.join(__dirname, 'sample-data', 'FP63 15-09-24 Eastern and S-East Freight NSP190824.pdf')
 const nspFP63EasternFreightCSV = (await fs.readFile(path.join(__dirname, 'sample-data', 'freight.csv'))).toString().replace(/^\uFEFF/, '').split('\n').map(line => line.split(','))
 
+const nspFP63AlburyPath = path.join(__dirname, 'sample-data', 'FP63 15-09-24 North Eastern Standard Gauge Full Service NSP130824.pdf')
+const nspFP63AlburyCSV = (await fs.readFile(path.join(__dirname, 'sample-data', 'albury.tsv'))).toString().replace(/^\uFEFF/, '').split('\n').map(line => line.split('\t'))
+
 describe('The PDF Table Reader class', () => {
-  it('Should convert a PDF buffer into an array of pages, each containing the table data', async () => {
+  it('Should convert a PDF buffer into an array of pages, each containing the table data (freight data)', async () => {
     let tableReader = new TableReader(nspFP63EasternFreightPath)
     let pages = await tableReader.read()
 
     expect(pages.length).to.equal(6)
     expect(pages[0]).to.deep.equal(nspFP63EasternFreightCSV)
+  })
+
+  it('Should convert a PDF buffer into an array of pages, each containing the table data (albury data)', async () => {
+    let tableReader = new TableReader(nspFP63AlburyPath)
+    let pages = await tableReader.read()
+
+    expect(pages.length).to.equal(4)
+    expect(pages[0]).to.deep.equal(nspFP63AlburyCSV)
   })
 })
