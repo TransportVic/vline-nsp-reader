3a8a6ee5b96420db60ea95996a19eb0b5d80ceeb
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-18 14:46:49 +1000

    add test for determining operation days

---
+++ test/nsp-utils.test.mjs
@@ -0,0 +1,27 @@
+import { expect } from 'chai'
+import { getOperationDays } from '../lib/nsp-utils.mjs'
+
+describe('The NSP Utils module', () => {
+  describe('The getOperationDays method', () => {
+    it('Should return all days for Daily', () => {
+      expect(getOperationDays('Daily')).to.deep.equal([ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ])
+    })
+    it('Should return Monday to Friday for MF', () => {
+      expect(getOperationDays('MF')).to.deep.equal([ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri' ])
+    })
+    it('Should only the specified day if it ends with O', () => {
+      expect(getOperationDays('Mo')).to.deep.equal([ 'Mon' ])
+      expect(getOperationDays('WThO')).to.deep.equal([ 'Wed', 'Thu' ])
+      expect(getOperationDays('SuO')).to.deep.equal([ 'Sun' ])
+    })
+    it('Should exclude days with an E', () => {
+      expect(getOperationDays('WThE')).to.deep.equal([ 'Mon', 'Tue', 'Fri', 'Sat', 'Sun' ])
+      expect(getOperationDays('FE')).to.deep.equal([ 'Mon', 'Tue', 'Wed', 'Thu', 'Sat', 'Sun' ])
+    })
+    it('Should join days with a +', () => {
+      expect(getOperationDays('Sun+Sat')).to.deep.equal([ 'Sat', 'Sun' ])
+      expect(getOperationDays('Sun+MF')).to.deep.equal([ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sun' ])
+      expect(getOperationDays('Sun+Sat+ME')).to.deep.equal([ 'Tue', 'Wed', 'Thu', 'Fri', 'Sun' ])
+    })
+  })
+})
