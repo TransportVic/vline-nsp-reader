189cdba57b5487afe0e94b4405203ddee45746f2
Eyeball Edward <edwardyeung39@gmail.com>
2025-06-18 17:38:31 +1000

    add sample test reader

---
+++ test.mjs
@@ -0,0 +1,47 @@
+import { getNSPVersion, NSPFile } from './lib/vline-nsp.mjs'
+import fs from 'fs/promises'
+import path from 'path'
+import url from 'url'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+let currentVersion = { effective: 0 }
+let nspFolder = path.join(__dirname, 'nsp-pdfs')
+let versionFile = path.join(nspFolder, 'ver.json')
+try {
+  currentVersion = JSON.stringify(await fs.readFile(versionFile))
+} catch (e) {}
+
+let nspFiles
+
+let latestVersion = { effective: 0, version: 'FP65' }
+// let latestVersion = (await getNSPVersion())[0]
+if (latestVersion.effective > currentVersion.effective) {
+  console.log(`Updating NSP to ${latestVersion.version}`)
+  if (await fs.stat(nspFolder)) await fs.rm(nspFolder, { recursive: true })
+  await fs.mkdir(nspFolder)
+
+  await latestVersion.saveFiles(nspFolder)
+  currentVersion = { version: latestVersion.version, effective: +latestVersion.effective }
+  await fs.writeFile(versionFile, JSON.stringify(currentVersion))
+
+  nspFiles = latestVersion.files
+} else {
+  console.log('NSP up to date')
+  let nspFileNames = await fs.readdir(nspFolder)
+  
+  nspFiles = []
+  for (let file of nspFileNames) {
+    if (!(file.endsWith('.pdf') && file.startsWith(latestVersion.version))) continue
+    let nspFile = new NSPFile(file.slice(0, -4), null, latestVersion.version)
+    nspFile.setFilePath(path.join(nspFolder, file))
+    nspFiles.push(nspFile)
+  }
+}
+
+for (let file of nspFiles) {
+  console.log('Reading', file)
+  console.log(await file.extractRuns())
+}
+
