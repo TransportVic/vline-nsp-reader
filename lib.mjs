1ec10d52687f8b9b9d69189aea2b2b6ae24c2fd5
Eyeball Edward <edwardyeung39@gmail.com>
2025-12-05 17:19:35 +0800

    fix last row issue

--- lib.mjs
+++ lib.mjs
@@ -1,9 +1,13 @@
+import { getHeatTimetables, HeatTimetable, HeatTimetableFile } from './lib/heat/vline-heat.mjs'
 import { getNSPVersion, NSPFile, NSPVersion } from './lib/nsp/vline-nsp.mjs'
 import PassPDFReader from './lib/pass/pass-pdf-reader.mjs'
 
 export {
   getNSPVersion,
+  getHeatTimetables,
   NSPFile,
   NSPVersion,
-  PassPDFReader
+  PassPDFReader,
+  HeatTimetable,
+  HeatTimetableFile
 }
